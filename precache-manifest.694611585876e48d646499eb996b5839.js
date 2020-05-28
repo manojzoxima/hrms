a006642093f3bf33237d2dc9e7cf2643d61108ac
Apple <Apple@Apples-MacBook-Pro.local>
2020-05-28 08:26:30 +0530

    Updates

---
+++ precache-manifest.694611585876e48d646499eb996b5839.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cd194ce39b000437834e779ccfd57ac",
+    "url": "/hrms/index.html"
+  },
+  {
+    "revision": "4193e5ab8ca790283794",
+    "url": "/hrms/static/css/2.8e7d2aed.chunk.css"
+  },
+  {
+    "revision": "4b4efe6b8671e1caf51e",
+    "url": "/hrms/static/css/main.9129e456.chunk.css"
+  },
+  {
+    "revision": "4193e5ab8ca790283794",
+    "url": "/hrms/static/js/2.0afaba1b.chunk.js"
+  },
+  {
+    "revision": "61774124b131ade046709435c72de1c2",
+    "url": "/hrms/static/js/2.0afaba1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b4efe6b8671e1caf51e",
+    "url": "/hrms/static/js/main.a1ab396a.chunk.js"
+  },
+  {
+    "revision": "f2adedd92f914d9d72b9",
+    "url": "/hrms/static/js/runtime-main.353eafff.js"
+  },
+  {
+    "revision": "61085cadf51af33041bddebbc675a617",
+    "url": "/hrms/static/media/auth_bg.61085cad.jpg"
+  },
+  {
+    "revision": "322d5f86374f7e4872fc7ecd2e57d7d4",
+    "url": "/hrms/static/media/avatar1.322d5f86.jpg"
+  },
+  {
+    "revision": "e93db838d8260c7cd1fd7da5d05f9bc9",
+    "url": "/hrms/static/media/avatar3.e93db838.jpg"
+  },
+  {
+    "revision": "8ff63a48076cde1db3e94696cfdf582a",
+    "url": "/hrms/static/media/avatar4.8ff63a48.jpg"
+  },
+  {
+    "revision": "4e8b017aef78cc114006cba896850fbd",
+    "url": "/hrms/static/media/dropify.4e8b017a.eot"
+  },
+  {
+    "revision": "86cb1ab018f1680d8e0fa0ca6a53d64c",
+    "url": "/hrms/static/media/dropify.86cb1ab0.ttf"
+  },
+  {
+    "revision": "950ffd1efa29c33a70dd86ea761cb3d8",
+    "url": "/hrms/static/media/dropify.950ffd1e.woff"
+  },
+  {
+    "revision": "b321d818cc153b16e739072ea8bc94b8",
+    "url": "/hrms/static/media/dropify.b321d818.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/hrms/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/hrms/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/hrms/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/hrms/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/hrms/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "f7447b196fb2095c74f4189748da5bc9",
+    "url": "/hrms/static/media/logo-white.f7447b19.svg"
+  },
+  {
+    "revision": "1bd10b76a6766833efa410b7776a4ab6",
+    "url": "/hrms/static/media/logo.1bd10b76.svg"
+  },
+  {
+    "revision": "596814caa4fbaecbf5014bcfe8e363fb",
+    "url": "/hrms/static/media/simple-Line-Icons.596814ca.ttf"
+  },
+  {
+    "revision": "c9c9b5ebc6395eeb83072bf18e3818aa",
+    "url": "/hrms/static/media/simple-Line-Icons.c9c9b5eb.svg"
+  },
+  {
+    "revision": "f19a7f6c7a0b54b748277c40d7cf8882",
+    "url": "/hrms/static/media/simple-Line-Icons.f19a7f6c.eot"
+  },
+  {
+    "revision": "ff94ad94c3a9d04bd2f80cb3c87dcccb",
+    "url": "/hrms/static/media/simple-Line-Icons.ff94ad94.woff"
+  },
+  {
+    "revision": "46c989feca5312ecf1368b4fbef5a6c8",
+    "url": "/hrms/static/media/user.46c989fe.png"
+  },
+  {
+    "revision": "f2cbc196c984d2fea2411dfba15e68a4",
+    "url": "/hrms/static/media/weather_img.f2cbc196.jpg"
+  },
+  {
+    "revision": "2ca3df0981116e195ebc62dd1dcc8b9b",
+    "url": "/hrms/static/media/weathericons-regular-webfont.2ca3df09.ttf"
+  },
+  {
+    "revision": "b2233bc1525939550405c87cffede242",
+    "url": "/hrms/static/media/weathericons-regular-webfont.b2233bc1.svg"
+  },
+  {
+    "revision": "e7ef2b448d27cf5312a73ceb3e7841c4",
+    "url": "/hrms/static/media/weathericons-regular-webfont.e7ef2b44.eot"
+  },
+  {
+    "revision": "f5b0d0d9cb02e551003132e079435312",
+    "url": "/hrms/static/media/weathericons-regular-webfont.f5b0d0d9.woff"
+  }
+]);
